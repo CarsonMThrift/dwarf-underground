73d79b0a90aa9bcf1842b0b3bf76e0db20259fff
Carson Thrift <Carson Thrift>
2017-06-13 15:20:20 -0400

    added component for other articles

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ import MainBody from './MainBody'
 import Advert from './Advert'
 import AuthorInfo from './AuthorInfo'
 import ArticleTitle from './ArticleTitle'
-
-
+import OtherArts from './OtherArts'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+
         <Header />
 
         <main className="expanded row">
@@ -26,33 +25,7 @@ class App extends Component {
 
           <Advert />
 
-          <div className="small-12 columns other-articles">
-            <h2>From around the Realm</h2>
-            <div className="small-6 medium-3 columns other-article">
-              <a href="#">
-                <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
-                <p>Single Orcs in Indianapolis</p>
-              </a>  
-            </div>
-            <div className="small-6 medium-3 columns other-article">
-              <a href="#">
-                <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
-                <p>You won't believe what's under this mountain</p>
-              </a>  
-            </div>
-            <div className="small-6 medium-3 columns other-article">
-              <a href="#">
-                <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
-                <p>Mine 20% more gold with One Weird Trick</p>
-              </a> 
-            </div>
-            <div className="small-6 medium-3 columns other-article">
-              <a href="#">
-                <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
-                <p>Surprise for Indiana Hobbits born before 1999</p>
-              </a> 
-            </div>
-          </div>
+          <OtherArts />
         </main>
 
         <footer className="expanded row">
